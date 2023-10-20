583fab7130d452b7141ebde88243199fa32668e8
Your Name <you@example.com>
2023-10-19 17:22:50 -0700

    step: add the cate

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -8,14 +8,24 @@ const Portfolio = () => {
       <h1 className={styles.selectTitle}>Choose a gallery</h1>
       <div className={styles.items}>
         <Link href="/portfolio/illustrations" className={styles.item}>
-          <span className={styles.title}>Illustrations</span>
+          <span className={styles.title}>Life</span>
         </Link>
         <Link href="/portfolio/test" className={styles.item}>
           <span className={styles.title}>Websites</span>
         </Link>
+        <Link href="/portfolio/poetry" className={styles.item}>
+          <span className={styles.title}>Poetry</span>
+        </Link>
+        {/* <Link href="/portfolio/application" className={styles.item}>
+          <span className={styles.title}>Application</span>
+        </Link>
         <Link href="/portfolio/application" className={styles.item}>
           <span className={styles.title}>Application</span>
         </Link>
+        <Link href="/portfolio/application" className={styles.item}>
+          <span className={styles.title}>Application</span>
+        </Link>
+         */}
       </div>
     </div>
   );
